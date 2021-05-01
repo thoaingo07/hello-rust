56156ab726781bf970a6916effbb21a194c53f75
tngo <thoaingo07@hotmail.com>
2021-05-01 12:29:56 +0700

    feat: copy and return with generic lifetime

---
+++ hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f.d
@@ -0,0 +1,5 @@
+/home/tngo/tngo_repos/github/hello-rust/hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f: src/main.rs
+
+/home/tngo/tngo_repos/github/hello-rust/hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/tngo/tngo_repos/github/hello-rust/hello_cargo/target/debug/hello_cargo: /home/tngo/tngo_repos/github/hello-rust/hello_cargo/src/main.rs
