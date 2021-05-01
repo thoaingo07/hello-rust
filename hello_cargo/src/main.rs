1c9bde3d689d99f6aca523ee687e9cf7b94323fa
tngo <thoaingo07@hotmail.com>
2021-05-01 13:43:24 +0700

    generic type

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,56 +1,19 @@
-trait AsJson {
-    fn as_json(&self) -> String;
-}
-//fn send_data_as_json<T: AsJson>(value: &T)
-fn send_data_as_json(value: &impl AsJson) {
-    println!("Sending JSON data to server...");
-    println!("-> {}", value.as_json());
-    println!("Done!\n");
+struct Container<T> {
+    value: T,
 }
 
-struct Person {
-    name: String,
-    age: u8,
-    favorite_fruit: String,
-}
-
-struct Dog {
-    name: String,
-    color: String,
-    likes_petting: bool,
-}
-
-impl AsJson for Person {
-    fn as_json(&self) -> String {
-        format!(
-            r#"{{ "type": "person", "name": "{}", "age": {}, "favoriteFruit": "{}" }}"#,
-            self.name, self.age, self.favorite_fruit
-        )
+impl<T> Container<T> {
+    pub fn new(value: T) -> Self {
+        Container { value }
     }
 }
 
-impl AsJson for Dog {
-    fn as_json(&self) -> String {
-        format!(
-            r#"{{ "type": "dog", "name": "{}", "color": "{}", "likesPetting": {} }}"#,
-            self.name, self.color, self.likes_petting
-        )
-    }
-}
-fn main(){
-    let laura = Person {
-        name: String::from("Laura"),
-        age: 31,
-        favorite_fruit: String::from("apples"),
-    };
-
-    let fido = Dog {
-        name: String::from("Fido"),
-        color: String::from("Black"),
-        likes_petting: true,
-    };
-
-    send_data_as_json(&laura);
-    send_data_as_json(&fido);
-
+fn main() {
+    assert_eq!(Container::new(42).value, 42);
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new("Foo").value, "Foo");
+    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(Container::new(true).value, true);
+    assert_eq!(Container::new(-12).value, -12);
+    assert_eq!(Container::new(Some("text")).value, Some("text"));
 }
